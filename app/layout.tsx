209b0cb82e03537f94ac6783ce03d21c46eb4027
Ganeshh <ganeshveleappa@gmail.com>
2026-03-08 12:28:47 +0530

    feat: integrate Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CustomCursor from "@/Components/cursor";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -46,6 +48,7 @@ export default function RootLayout({
       >
         <CustomCursor />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
